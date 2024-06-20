b5ba05f1ddb71f357e21d1577795eb24a082834d
Dario Lo Forti <lofortidario@gmail.com>
2024-06-20 08:57:47 +0200

    Update dark mode functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ export default function() {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            document.body.style.backgroundColor = document.body.style.backgroundColor === 'white' ? '#383737' : 'white';
-            const textColor = document.body.style.backgroundColor === 'white' ? 'black' : 'white';
-            document.body.style.color = textColor;
-            const labels = document.querySelectorAll('label, h1, h3, p, li');
-            labels.forEach(label => label.style.color = textColor);
+            const body = document.body;
+            if (body.classList.contains('light-mode')) {
+                body.classList.replace('light-mode', 'dark-mode');
+            } else {
+                body.classList.replace('dark-mode', 'light-mode');
+            }
         }, 5000);
+
+        
+        document.body.classList.add('dark-mode');
 
         return () => clearInterval(intervalId);
     }, []);
